126afe8104bd2a677f46397259e96e5abc57a02a
anatolij <anatolijzdanov4@gmail.com>
2023-06-11 01:55:23 +0300

    написалаи решение task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход число (N) а на выходе показывает все четные числа.
+
+System.Console.WriteLine("Введите число");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int index = -number;
+while(index <= number)
+{
+if((index % 2) == 0)
+    System.Console.WriteLine(index + " ");
+index = index + 1;
+}
+
