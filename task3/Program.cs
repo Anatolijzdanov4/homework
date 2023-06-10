944fd740971672449a301dde699b86c43bd2f026
anatolij <anatolijzdanov4@gmail.com>
2023-06-11 00:46:12 +0300

    написалаи решение task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// 
+
+System.Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if((Number % 2) == 0)
+{
+    System.Console.WriteLine("Четное");
+}
+else 
+{
+    System.Console.WriteLine("Не четое");
+}
