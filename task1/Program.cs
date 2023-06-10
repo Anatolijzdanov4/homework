4fb9feab4b04fa319c83030ba88c0ff23819ddc8
anatolij <anatolijzdanov4@gmail.com>
2023-06-10 23:34:40 +0300

    написалаи решение task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу которая принимает на вход 2 числа и выдает какое число больше а какое меньше
+
+System.Console.WriteLine("Введите первое число");
+int Number1 = Convert.ToInt16(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int Number2 = Convert.ToInt16(Console.ReadLine());
+if (Number1 > Number2)
+{
+    System.Console.WriteLine(Number1);
+}
+else
+{
+    System.Console.WriteLine(Number2);
+}
